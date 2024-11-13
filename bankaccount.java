aedeefc5bcd96ccfa1bca378e5d36989a9bf3b98
Sai Reddy <124039319+192211170@users.noreply.github.com>
2024-11-13 12:13:02 +0530

    Add files via upload

--- bankaccount.java
+++ bankaccount.java
@@ -1,53 +1,39 @@
-package day2;
-import java.util.Scanner;
+package day8;
+class Bank{
+	String name,type;
+	double balance;
+	int acc_num;
+	public Bank(String n,int a_n,String t,double b) {
+		this.name = n;
+		this.balance = b;
+		this.acc_num = a_n;
+		this.type = t;
+	}
+	public void deposit(double amt) {
+		if(amt>0) {
+			balance+=amt;
+		}
+		else
+			System.out.println("Invalid ammount.");
+	}
+	public void withdraw(double amt) {
+		if(amt>0 && amt<=balance) {
+			balance-=amt;
+		}
+		else
+			System.out.println("Insufficient balance.");
+	}
+	public void display() {
+		System.out.println("Name :"+name);
+		System.out.println("Balance :"+balance);
+	}
+}
 public class bankaccount {
-	public static void main(String args[]) {
-		Scanner a = new Scanner(System.in);
-		double bal = 0;
-		int choice;
-		do {
-		System.out.println("\nBANK ACCOUNT MENU");
-		System.out.println("1.check balance");
-		System.out.println("2.deposit money");
-		System.out.println("3.withdraw money");
-		System.out.println("4.Exit");
-		System.out.println("Choose an option:");
-		choice = a.nextInt();
-			switch(choice) {
-			case 1:
-				System.out.println("Your balance :Rs"+bal);
-				break;
-			case 2:
-				System.out.println("Enter amount to deposit:");
-				double dep = a.nextDouble();
-				if(dep>0) {
-					bal+=dep;
-					System.out.println("Amount deposited:Rs"+dep);
-					break;
-				}
-				else {
-					System.out.println("Enter valid value for amount.");
-				}
-			case 3:
-				System.out.println("Enter amount to withdraw:");
-				double wiamt = a.nextDouble();
-				if(wiamt>0 && wiamt<=bal) {
-					bal-=wiamt;
-					System.out.println("Amount withdrawed:Rs"+wiamt);
-				}
-				else if(wiamt>bal) {
-					System.out.println("Insufficient balance.");
-				}
-				else {
-					System.out.println("Enter a valid value to withdraw.");
-				}
-			case 4:
-				System.out.println("Thankyou for using!");
-				break;
-			
-			}
-		}while(choice!=4);
-		
+	public static void main(String[] args) {
+		Bank b1 = new Bank("koushik",192211,"savings bank",2000);
+		b1.display();
+		Bank b2 = new Bank("lathika",192210,"premium bank",4000);
+		b2.display();
 	}
 
 }
