fa1d6e9fd4f41a05ece8413cae80d970b75b73c1
Sai Reddy <124039319+192211170@users.noreply.github.com>
2024-11-13 12:55:59 +0530

    Add files via upload

---
+++ sumnatural.java
@@ -0,0 +1,11 @@
+public class sumnatural{
+	public static void main(String args[]){
+		int n=10;
+		int sum=0,i=1;
+		while(i<=n){
+			sum+=i;
+			i++;
+		}
+		System.out.println("sum:"+ sum);
+	}
+}
